cef47c813efd2b6b73d160985c4cfa185dc96c5b
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 08:50:03 +0900

    Add routing and create HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
+import HomePage from "./pages/HomePage";
 
 export function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
