aa57c09643dcff1e2b8b358ac67feed10142a18f
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 08:57:27 +0900

    Format

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { StrictMode } from "react";
 
 function start() {
   const element = document.getElementById("root");
