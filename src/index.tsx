e21b0164a03d57500da6a6618be6c8ecbd684555
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 08:45:18 +0900

    Add null check for root element in React rendering

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 function start() {
-  const root = createRoot(document.getElementById("root")!);
+  const element = document.getElementById("root");
+  if (!element) {
+    throw new Error("Root element not found");
+  }
+
+  const root = createRoot(element);
   root.render(<App />);
 }
 
