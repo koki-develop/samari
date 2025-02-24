f462e8a1a65549d79e0476210096ca1377b8b883
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 11:02:41 +0900

    Implement responsive AppShell layout with mobile and desktop navigation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,53 @@
+import Logo from "@/assets/logo/logo.png";
+import { ActionIcon, AppShell, Drawer } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconMenu2 } from "@tabler/icons-react";
 import { Outlet } from "react-router";
 
 export default function Layout() {
+  const [opened, { toggle, close }] = useDisclosure();
+
   return (
-    <div>
-      Layout
-      <Outlet />
-    </div>
+    <AppShell
+      classNames={{ header: "flex items-center gap-3 px-4" }}
+      header={{ height: 60 }}
+      navbar={{
+        width: 250,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <ActionIcon
+          c="gray"
+          hiddenFrom="sm"
+          onClick={toggle}
+          variant="transparent"
+        >
+          <IconMenu2 />
+        </ActionIcon>
+        <img className="h-7" src={Logo} alt="SAMARI" />
+      </AppShell.Header>
+
+      {/* navbar for desktop */}
+      <AppShell.Navbar visibleFrom="sm">NAVBAR_FOR_DESKTOP</AppShell.Navbar>
+
+      {/* navbar for mobile */}
+      <Drawer
+        styles={{
+          header: { borderBottom: "1px solid var(--mantine-color-gray-3)" },
+        }}
+        size="xs"
+        hiddenFrom="sm"
+        opened={opened}
+        onClose={close}
+      >
+        NAVBAR_FOR_MOBILE
+      </Drawer>
+
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
   );
 }
