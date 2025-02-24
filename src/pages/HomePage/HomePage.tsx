f92d821b2f7bfd4353ef61656b9c7d7f0b533809
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 14:01:41 +0900

    Add pagination support to HomePage with loading and "load more" functionality

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,34 @@
 import { usePosts } from "@/lib/post";
 import { useActivePostGroup } from "@/lib/postGroup";
-import { Box } from "@mantine/core";
+import { Box, Button, Loader } from "@mantine/core";
 import PostList from "./PostList";
 import PostListItem from "./PostListItem";
 
 export default function HomePage() {
   const activePostGroup = useActivePostGroup();
-  const { posts } = usePosts(activePostGroup?.name ?? null);
+  const { posts, isFetching, hasNextPage, fetchNextPage } = usePosts(
+    activePostGroup?.name ?? null,
+  );
 
   return (
-    <Box className="p-4">
+    <Box className="flex flex-col gap-4 p-4">
       <PostList>
         {posts.map((post) => (
           <PostListItem key={post.id} post={post} />
         ))}
       </PostList>
+
+      {isFetching && (
+        <Box className="flex justify-center">
+          <Loader type="dots" />
+        </Box>
+      )}
+
+      {!isFetching && hasNextPage && (
+        <Box className="flex justify-center">
+          <Button onClick={() => fetchNextPage()}>もっと見る</Button>
+        </Box>
+      )}
     </Box>
   );
 }
