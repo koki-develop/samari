4077c420596777453b3744a710335f95f0ed1f66
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 15:16:12 +0900

    Migrate to React Router v6.4+ with createBrowserRouter and ScrollRestoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { RouterProvider, createBrowserRouter } from "react-router";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 
@@ -13,17 +13,24 @@ const queryClient = new QueryClient({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </MantineProvider>
     </QueryClientProvider>
   );
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,59 +2,63 @@ import Logo from "@/assets/logo/logo.png";
 import { ActionIcon, Anchor, AppShell, Drawer } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconMenu2 } from "@tabler/icons-react";
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, ScrollRestoration } from "react-router";
 import NavList from "./NavList";
 
 export default function Layout() {
   const [opened, { toggle, close }] = useDisclosure();
 
   return (
-    <AppShell
-      classNames={{ header: "flex items-center gap-3 px-4" }}
-      header={{ height: 60 }}
-      navbar={{
-        width: 250,
-        breakpoint: "sm",
-        collapsed: { mobile: !opened },
-      }}
-    >
-      <AppShell.Header>
-        <ActionIcon
-          c="gray"
-          hiddenFrom="sm"
-          onClick={toggle}
-          variant="transparent"
-        >
-          <IconMenu2 />
-        </ActionIcon>
-        <Anchor classNames={{ root: "flex" }} component={Link} to="/">
-          <img className="h-7" src={Logo} alt="SAMARI" />
-        </Anchor>
-      </AppShell.Header>
-
-      {/* navbar for desktop */}
-      <AppShell.Navbar visibleFrom="sm">
-        <NavList />
-      </AppShell.Navbar>
-
-      {/* navbar for mobile */}
-      <Drawer
-        classNames={{
-          body: "p-0 flex-grow flex flex-col",
-          content: "flex flex-col",
-          header: "border-b border-solid border-[var(--mantine-color-gray-3)]",
+    <>
+      <ScrollRestoration />
+      <AppShell
+        classNames={{ header: "flex items-center gap-3 px-4" }}
+        header={{ height: 60 }}
+        navbar={{
+          width: 250,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
         }}
-        size="xs"
-        hiddenFrom="sm"
-        opened={opened}
-        onClose={close}
       >
-        <NavList />
-      </Drawer>
+        <AppShell.Header>
+          <ActionIcon
+            c="gray"
+            hiddenFrom="sm"
+            onClick={toggle}
+            variant="transparent"
+          >
+            <IconMenu2 />
+          </ActionIcon>
+          <Anchor classNames={{ root: "flex" }} component={Link} to="/">
+            <img className="h-7" src={Logo} alt="SAMARI" />
+          </Anchor>
+        </AppShell.Header>
 
-      <AppShell.Main>
-        <Outlet />
-      </AppShell.Main>
-    </AppShell>
+        {/* navbar for desktop */}
+        <AppShell.Navbar visibleFrom="sm">
+          <NavList />
+        </AppShell.Navbar>
+
+        {/* navbar for mobile */}
+        <Drawer
+          classNames={{
+            body: "p-0 flex-grow flex flex-col",
+            content: "flex flex-col",
+            header:
+              "border-b border-solid border-[var(--mantine-color-gray-3)]",
+          }}
+          size="xs"
+          hiddenFrom="sm"
+          opened={opened}
+          onClose={close}
+        >
+          <NavList />
+        </Drawer>
+
+        <AppShell.Main>
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
+    </>
   );
 }
