4c7d5554e7e42475009e60d20f2d6f0443d02dd2
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 09:47:37 +0900

    Configure React Query with default options in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import "@mantine/core/styles.css";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export function App() {
   return (
-    <MantineProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
 
