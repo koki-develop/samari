8ffc23cc7c5d6e24e2f64d72c06c62e74e86c7e3
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 13:08:31 +0900

    Add Firebase and contact form environment variable types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,3 +7,17 @@ declare module "*.svg" {
   const value: string;
   export default value;
 }
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly BUN_PUBLIC_FIREBASE_API_KEY: string;
+    readonly BUN_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    readonly BUN_PUBLIC_FIREBASE_PROJECT_ID: string;
+    readonly BUN_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    readonly BUN_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly BUN_PUBLIC_FIREBASE_APP_ID: string;
+
+    readonly BUN_PUBLIC_CONTACT_FORM_URL: string;
+    readonly BUN_PUBLIC_FEATURE_REQUEST_FORM_URL: string;
+  }
+}
