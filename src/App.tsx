c1ef33ecef646af7ca394059ac450caf4741534f
koki-develop <kou.pg.0131@gmail.com>
2025-02-24 12:55:39 +0900

    Update CSS imports and layer configuration for Mantine and Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "@mantine/core/styles.css";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
